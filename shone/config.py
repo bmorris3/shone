92afec73fe25439b6c04099cd114f0179c0f29c2
Brett M. Morris <bmmorris@stsci.edu>
2025-04-10 12:03:21 -0400

    following jax config for default float precision

--- shone/config.py
+++ shone/config.py
@@ -1,11 +1,11 @@
 import os
-from jax import numpy as jnp
+from jax import numpy as jnp, config as jax_config
 
 on_rtd = os.getenv('READTHEDOCS', False)
 
 shone_dir = os.path.expanduser(os.path.join("~", ".shone"))
 tiny_archives_dir = os.path.join(shone_dir, 'tiny_archives')
-float_dtype = jnp.float64
+float_dtype = jnp.float64 if jax_config.read('jax_enable_x64') else jnp.float32
 
 if on_rtd:
     # use a temporary directory on readthedocs:
