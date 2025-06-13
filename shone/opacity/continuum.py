continuum shape fixes, add missing factor of 2

shone/opacity/continuum.py
@@ -37,26 +37,25 @@ def h_minus_continuum(
     .. [1] `John, T. L. 1988, Astronomy and Astrophysics, 193, 189
            <https://ui.adsabs.harvard.edu/abs/1988A%26A...193..189J/abstract>`_
     """
-    temperature = jnp.atleast_1d(temperature)
-
     electron_pressure = (
         number_density_e * k_B * temperature
     )  # [dyn/cm2]
 
     log_cross_section = jnp.log10(
-        electron_pressure *  # [dyn/cm2]
+        electron_pressure[:, None] *  # [dyn/cm2]
         (
             free_free_absorption(wavelength, temperature) +  # [cm4/dyn]
-            bound_free_absorption(wavelength) * vmr_h1minus  # [cm4/dyn]
+            bound_free_absorption(wavelength)[None, :] *
+            vmr_h1minus[:, None]  # [cm4/dyn]
         )
     )  # [cm2]
 
     log_absorption_coeff = (
         26 + log_cross_section +
-        jnp.log10(number_density_h)
+        jnp.log10(number_density_h[:, None])
     )
 
-    return 10 ** log_absorption_coeff.T  # [1/cm]
+    return 10 ** log_absorption_coeff  # [1/cm]
 
 
 @jit

shone/opacity/tests/test_continuum.py
@@ -1,18 +1,22 @@
+import pytest
 from shone.constants import k_B
 from shone.opacity.continuum import h_minus_continuum
 import jax.numpy as jnp
 
 
-def test_h_minus_continuum(mihalas_h_minus_continuum):
+@pytest.mark.parametrize(
+    "n_pressure_layers,", (1, 2)
+)
+def test_h_minus_continuum(n_pressure_layers, mihalas_h_minus_continuum):
     # number densities and volume mixing ratios were computed
     # with fastchem using:
     #    temperature = 6300 K
     #    pressure = 1 bar
-    number_density_e = 188856770172237.6
-    number_density_H = 1.0586659319005289e+18
-    vmr_H = 0.920837017933896
-    vmr_h1minus = 1.445164249467722e-07
-    temperature = 6300  # [K]
+    number_density_e = jnp.array(n_pressure_layers * [188856770172237.6])
+    number_density_H = jnp.array(n_pressure_layers * [1.0586659319005289e+18])
+    vmr_H = jnp.array(n_pressure_layers * [0.920837017933896])
+    vmr_h1minus = jnp.array(n_pressure_layers * [1.445164249467722e-07])
+    temperature = jnp.array(n_pressure_layers * [6300])  # [K]
 
     electron_pressure = (
         number_density_e * k_B * temperature
@@ -24,7 +28,7 @@ def test_h_minus_continuum(mihalas_h_minus_continuum):
     # electron pressure per H atom.
     mihalas_wl_1000AA, mihalas_alpha = mihalas_h_minus_continuum
     mihalas_wl_um = mihalas_wl_1000AA * 0.1  # [µm]
-    mihalas_absorption_coeff = mihalas_alpha * electron_pressure * number_density_H  # [cm-1]
+    mihalas_absorption_coeff = mihalas_alpha[:, None] * (electron_pressure * number_density_H)[None, :]  # [cm-1]
 
     shone_absorption_coeff = h_minus_continuum(
         mihalas_wl_um, temperature,
@@ -33,4 +37,5 @@ def test_h_minus_continuum(mihalas_h_minus_continuum):
     ).squeeze()  # [cm-1]
 
     # median absolute deviation should be <10%:
-    assert jnp.median(jnp.abs(shone_absorption_coeff / mihalas_absorption_coeff - 1)) < 0.10
+    assert jnp.median(jnp.abs(shone_absorption_coeff / mihalas_absorption_coeff.T - 1)) < 0.10
+

shone/transmission/de_wit_seager_2013.py
@@ -161,7 +161,7 @@ def _transmission_radius(
     # ensure that this is a lower triangular.
     # flip result's order along the pressure dimension from high-to-low (increasing `r`)
     # to low-to-high (like `pressure`).
-    dx = jnp.tril(-jnp.diff(x, axis=1))[::-1]
+    dx = 2 * jnp.tril(-jnp.diff(x, axis=1))[::-1]
 
     # compute the optical depth due to scattering from the absorption coefficient
     # for the scattering species: